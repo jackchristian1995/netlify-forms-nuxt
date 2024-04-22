b780cd712436a5124badc5063963790cabeb7b4d
Jack Christian <ccontact@jackchristian.uk>
2024-04-22 16:49:43 +1000

    configure prerender

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  pages: true,
+  nitro: {
+    prerender: {
+      routes: [
+        '/'
+      ]
+    }
+  },
   css: ['~/assets/style.css']
 })
